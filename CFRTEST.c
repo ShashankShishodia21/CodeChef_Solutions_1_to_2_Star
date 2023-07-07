dce24fafbbb79536cc1c93780facdb2a22be3d14
Shashank Shishodia <115582986+ShashankShishodia21@users.noreply.github.com>
2023-07-07 22:16:13 +0530

    Create CFRTEST.c

---
+++ CFRTEST.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	int t;
+	scanf("%d",&t);
+	while(t--){
+	    int n;
+	    int ans=0;
+	    scanf("%d",&n);
+	    int arr[n];
+	    for(int i=1;i<=n;i++){
+	        scanf("%d",&arr[i]);
+	    }
+	    for(int i=1;i<=n;i++)
+	        for(int j=i+1;j<=n;j++){
+	        if(arr[i]==arr[j]){
+	            ans++;
+	            break;
+	        }
+	    }
+	    printf("%d\n",n-ans);
+	}
+	return 0;
+}
+
