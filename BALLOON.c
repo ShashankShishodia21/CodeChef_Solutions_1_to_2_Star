698fe0b0b5b99d0f700c48105c8b9201106d7d4a
Shashank Shishodia <115582986+ShashankShishodia21@users.noreply.github.com>
2023-07-09 15:14:33 +0530

    Create BALLOON.c

---
+++ BALLOON.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	int t;
+	scanf("%d",&t);
+	while(t--){
+	    int n,ans,a=0;
+	    scanf("%d",&n);
+	    int arr[n];
+	    for(int i=1;i<=n;i++){
+	        scanf("%d",&arr[i]);
+	    }
+	    for(int i=1;i<=n;i++){
+	        if(arr[i]<=7 && arr[i]>=1){
+	            a++;
+	            if(a==7){
+	                ans = i;
+	                break;
+	            }
+	        }
+	    }
+	    printf("%d\n",ans);
+	}
+	return 0;
+}
+
